b98aa7ea66e543132809733a6d85da6d1cd5145a
Justin Pfeifer <Justin.Pfeifer@dixie.edu>
2020-05-22 16:22:18 -0600

    syncing with master

---
+++ dws_create_table.sql
@@ -0,0 +1,17 @@
+create table USHE_DWS (
+    D_Inst varchar2(4),
+    D_SSN varchar2(15),
+    D_Banner_ID varchar2(6),
+    D_Start_DT varchar2(8),
+    D_End_DT varchar2(8),
+    Description varchar2(50),
+    YRQTR varchar2(5),
+    Wages number,
+    NAICS_CD varchar2(50),
+    Submission_DT date,
+    Return_DT date
+);
+
+
+
+
