5c0110be0ceab4bf44ec0cbdf63f85651b6e23da
Justin Pfeifer <Justin.Pfeifer@dixie.edu>
2020-07-13 10:14:13 -0600

    updated sql style

--- dws_submit_file.sql
+++ dws_submit_file.sql
@@ -1,16 +1,16 @@
-select
-3671 as D_INST,
-spbpers_ssn as D_SSN,
-spriden_id as D_BANNER_ID,
-to_char(shrdgmr_grad_date, 'yyyymmdd') as D_START_DT,
-'20200331' as D_END_DT,
-shrdgmr_program as Description
-from saturn.shrdgmr, spriden, spbpers
-where shrdgmr_pidm = spriden_pidm
-  and shrdgmr_pidm = spbpers_pidm
-  and shrdgmr_degs_code = 'AW'
-  and spriden_change_ind is null
-  and spbpers_ssn is not null
-  and shrdgmr_grad_date between to_date('2012', 'yyyy')
-  and to_date('06/30/2020', 'mm/dd/yyyy')
-order by 4 asc;
+SELECT 3671 AS d_inst,
+       spbpers_ssn AS d_ssn,
+       spriden_id AS d_banner_id,
+       to_char(shrdgmr_grad_date, 'yyyymmdd') AS d_start_dt,
+       '20200331' AS d_end_dt,
+       shrdgmr_program AS description
+  FROM saturn.shrdgmr,
+       spriden,
+       spbpers
+ WHERE shrdgmr_pidm = spriden_pidm
+   AND shrdgmr_pidm = spbpers_pidm
+   AND shrdgmr_degs_code = 'AW'
+   AND spriden_change_ind IS NULL
+   AND spbpers_ssn IS NOT NULL
+   AND shrdgmr_grad_date BETWEEN to_date('2012', 'yyyy') AND to_date('06/30/2020', 'mm/dd/yyyy')
+ ORDER BY 4 ASC;
