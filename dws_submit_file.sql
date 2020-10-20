1ee26cf1fdf7096c25db7eb5bc4a28f29696375f
Justin Pfeifer <Justin.Pfeifer@dixie.edu>
2020-10-20 08:52:09 -0600

    updated to 3rd quarter

--- dws_submit_file.sql
+++ dws_submit_file.sql
@@ -2,7 +2,7 @@ SELECT 3671 AS d_inst,
        spbpers_ssn AS d_ssn,
        spriden_id AS d_banner_id,
        to_char(shrdgmr_grad_date, 'yyyymmdd') AS d_start_dt,
-       '20200630' AS d_end_dt, -- update this to match WHERE clause
+       '20200930' AS d_end_dt, -- update this to match WHERE clause
        shrdgmr_program AS description
   FROM saturn.shrdgmr,
        spriden,
@@ -12,5 +12,5 @@ SELECT 3671 AS d_inst,
    AND shrdgmr_degs_code = 'AW'
    AND spriden_change_ind IS NULL
    AND spbpers_ssn IS NOT NULL
-   AND shrdgmr_grad_date BETWEEN to_date('2012', 'yyyy') AND to_date('06/30/2020', 'mm/dd/yyyy')
+   AND shrdgmr_grad_date BETWEEN to_date('2012', 'yyyy') AND to_date('09/30/2020', 'mm/dd/yyyy')
  ORDER BY 4 ASC;
